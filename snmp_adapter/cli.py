d147e0be3fee042c1485ccf59ebcf51a93fd97bd
Krys Lawrence <krys@krys.ca>
2020-11-28 17:36:47 -0500

    Got pysnmp working, even with extra MIBs.

--- snmp_adapter/cli.py
+++ snmp_adapter/cli.py
@@ -5,15 +5,49 @@ import sys
 
 import click
 
+from snmp_adapter import snmp_adapter
 
-@click.command()
-def main(args=None):
+
+class AliasedGroup(click.Group):
+    def get_command(self, ctx, cmd_name):
+        rv = click.Group.get_command(self, ctx, cmd_name)
+        if rv is not None:
+            return rv
+        matches = [x for x in self.list_commands(ctx) if x.startswith(cmd_name)]
+        if not matches:
+            return None
+        elif len(matches) == 1:
+            return click.Group.get_command(self, ctx, matches[0])
+        ctx.fail("Too many matches: %s" % ", ".join(sorted(matches)))
+
+
+@click.group(cls=AliasedGroup, invoke_without_command=True, chain=True)
+@click.pass_context
+def main(ctx):
+    if ctx.invoked_subcommand is None:
+        default()
+
+
+@main.command()
+def default(args=None):
     """Console script for snmp_adapter."""
     click.echo(
         "Replace this message by putting your code into " "snmp_adapter.cli.main"
     )
     click.echo("See click documentation at http://click.pocoo.org/")
     return 0
+
+
+@main.command()
+def quickstart():
+    """PySNMP Quick Start Example"""
+    return snmp_adapter.quickstart()
+
+
+@main.command()
+def common():
+    """PySNMP Tutorial Common Operations Example"""
+    return snmp_adapter.common()
 
 
 if __name__ == "__main__":
--- snmp_adapter/snmp_adapter.py
+++ snmp_adapter/snmp_adapter.py
@@ -1,3 +1,50 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+from pysnmp import hlapi
+
+
+def quickstart():
+    iterator = hlapi.getCmd(
+        hlapi.SnmpEngine(),
+        hlapi.CommunityData("public", mpModel=0),
+        hlapi.UdpTransportTarget(("192.168.0.59", 161)),
+        hlapi.ContextData(),
+        hlapi.ObjectType(hlapi.ObjectIdentity("SNMPv2-MIB", "sysDescr", 0)),
+    )
+    errorIndication, errorStatus, errorIndex, varBinds = next(iterator)
+    if errorIndication:
+        print(errorIndication)
+        return 1
+    elif errorStatus:
+        print(
+            "%s at %s"
+            % (
+                errorStatus.prettyPrint(),
+                errorIndex and varBinds[int(errorIndex) - 1][0] or "?",
+            )
+        )
+        return 1
+    else:
+        for varBind in varBinds:
+            print(" = ".join([x.prettyPrint() for x in varBind]))
+        return 0
+
+
+def common():
+    engine = hlapi.SnmpEngine()
+    community = hlapi.CommunityData("public", mpModel=1)  # SNMPv2c
+    target = hlapi.UdpTransportTarget(("192.168.0.59", 161))
+    context = hlapi.ContextData()
+
+    # object_id = hlapi.ObjectIdentity(
+    #     "iso.org.dod.internet.mgmt.mib-2.system.sysDescr.0"
+    # )
+    sysDescr = hlapi.ObjectType(
+        hlapi.ObjectIdentity("SNMPv2-MIB", "sysDescr", 0)
+    ).loadMibs("IF-MIB")
+    sysUpTime = hlapi.ObjectType(hlapi.ObjectIdentity("SNMPv2-MIB", "sysUpTime", 0))
+    foo = hlapi.ObjectType(hlapi.ObjectIdentity("IF-MIB", "ifInOctets", 1))
+    command = hlapi.getCmd(engine, community, target, context, sysDescr, sysUpTime, foo)
+    print(next(command))
